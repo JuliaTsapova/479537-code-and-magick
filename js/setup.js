e5c81605dcb5f9c0914fc647da566cdc90d72577
Julia Tsapova <12tsapova@gmail.com>
2017-11-27 02:07:17 +0700

    commit

---
+++ js/setup.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var wizadrs = [];
+var count = 4;
+var fame = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристов', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
+var surname = [' да Марья', ' Верон', ' Мирабелла', ' Вальц', ' Онопко', ' Топольницкая', ' Нионго', ' Ирвинг'];
+var coatColor = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
+var eyesColor = ['black', 'red', 'blue', 'yellow', 'green'];
+var similarListElement = document.querySelector('.setup-similar-list');
+var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+var fragment = document.createDocumentFragment();
+
+var getRandomKey = function (arr) {
+  return Math.floor(Math.random() * arr.length);
+};
+
+var renderWizard = function (data) {
+  var wizard = similarWizardTemplate.cloneNode(true);
+  wizard.querySelector('.setup-similar-label').textContent = data.fame;
+  wizard.querySelector('.wizard-coat').style.fill = data.coatColor;
+  wizard.querySelector('.wizard-eyes').style.fill = data.eyesColor;
+  return wizard;
+};
+
+while (--count >= 0) {
+  wizadrs.push({
+    fame: fame[getRandomKey(fame)] + surname[getRandomKey(surname)],
+    coatColor: coatColor[getRandomKey(coatColor)],
+    eyesColor: eyesColor[getRandomKey(eyesColor)]
+  });
+}
+
+for (var i = 0; i < wizadrs.length; i++) {
+  fragment.appendChild(renderWizard(wizadrs[i]));
+}
+
+similarListElement.appendChild(fragment);
+
+document.querySelector('.setup').classList.remove('hidden');
+document.querySelector('.setup-similar').classList.remove('hidden');
