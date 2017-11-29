73a752cf88ab992e2294d2c9ca9a22d04c0c01fa
Julia Tsapova <12tsapova@gmail.com>
2017-11-29 21:36:32 +0700

    Учебный проект: нас орда(готовый вариант)

--- js/setup.js
+++ js/setup.js
@@ -9,7 +9,6 @@ var wizardParams = {
 };
 var similarListElement = document.querySelector('.setup-similar-list');
 var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
-var fragment = document.createDocumentFragment();
 
 var getRandomNumber = function (number) {
   return Math.floor(Math.random() * number);
@@ -17,25 +16,37 @@ var getRandomNumber = function (number) {
 
 var renderWizard = function (data) {
   var wizard = similarWizardTemplate.cloneNode(true);
-  wizard.querySelector('.setup-similar-label').textContent = data.firstName;
+  wizard.querySelector('.setup-similar-label').textContent = data.name;
   wizard.querySelector('.wizard-coat').style.fill = data.coatColor;
   wizard.querySelector('.wizard-eyes').style.fill = data.eyesColor;
   return wizard;
 };
 
-while (--COUNT >= 0) {
-  wizadrs.push({
-    firstName: firstName[getRandomKey(firstName)] + surname[getRandomKey(surname)],
-    coatColor: coatColor[getRandomKey(coatColor)],
-    eyesColor: eyesColor[getRandomKey(eyesColor)]
-  });
-}
+var getWizard = function () {
+  return {
+    name: wizardParams.FIRST_NAME[getRandomNumber(wizardParams.FIRST_NAME.length)] + wizardParams.SURNAME[getRandomNumber(wizardParams.SURNAME.length)],
+    coatColor: wizardParams.COAT_COLOR[getRandomNumber(wizardParams.COAT_COLOR.length)],
+    eyesColor: wizardParams.EYES_COLOR[getRandomNumber(wizardParams.EYES_COLOR.length)]
+  };
+};
 
-for (var i = 0; i < wizadrs.length; i++) {
-  fragment.appendChild(renderWizard(wizadrs[i]));
-}
+var getWizardsArray = function (arrLength) {
+  var wizards = [];
+  for (var i = 0; i < arrLength; i++) {
+    wizards.push(getWizard());
+  }
+  return wizards;
+};
 
-similarListElement.appendChild(fragment);
+var createFragment = function (arr) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < arr.length; i++) {
+    fragment.appendChild(renderWizard(arr[i]));
+  }
+  return fragment;
+};
+
+similarListElement.appendChild(createFragment(getWizardsArray(COUNT)));
 
 document.querySelector('.setup').classList.remove('hidden');
 document.querySelector('.setup-similar').classList.remove('hidden');
