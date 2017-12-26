66d9c2d80033ec5706cd6539d372fb579e0f30d7
Julia Tsapova <12tsapova@gmail.com>
2017-12-27 04:08:46 +0700

    Учебный проект: в движении

--- js/setup.js
+++ js/setup.js
@@ -6,6 +6,14 @@
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var draggedItem = null;
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var setupStyle = window.getComputedStyle(setup);
+  var defaultCoords = {
+    x: setupStyle.left,
+    y: setupStyle.top
+  };
 
   var onPopupEscPress = function (evt) {
     if (evt.keyCode === ESC_KEYCODE && !(evt.target.nodeName === 'INPUT' && evt.target.className === 'setup-user-name')) {
@@ -20,6 +28,8 @@
 
   var closePopup = function () {
     setup.classList.add('hidden');
+    setup.style.top = defaultCoords.y;
+    setup.style.left = defaultCoords.x;
     document.removeEventListener('keydown', onPopupEscPress);
   };
 
@@ -37,5 +47,35 @@
     if (evt.keyCode === ENTER_KEYCODE) {
       closePopup();
     }
+  });
+
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName.toLowerCase() === 'img') {
+      draggedItem = evt.target.cloneNode(true);
+      evt.dataTransfer.setData('text/plain', evt.target.alt);
+      artifactsElement.style.outline = '2px dotted red';
+    }
+  });
+
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
+
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.target.style.backgroundColor = '';
+    artifactsElement.style.outline = '';
+    evt.target.appendChild(draggedItem);
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    evt.target.style.backgroundColor = 'yellow';
+    evt.preventDefault();
+  });
+
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.style.backgroundColor = '';
+    evt.preventDefault();
   });
 })();
